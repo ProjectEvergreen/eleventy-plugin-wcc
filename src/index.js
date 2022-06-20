30a91202405ba0a428e6f0686f708a0863df0637
Owen Buckley <owenbuckley13@gmail.com>
2022-06-20 18:12:41 -0400

    add netlify configuration and github actions (#1)
    * add netlify configuration and github actions
    
    * fix github actions workflows location

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ module.exports = {
     const { definitions = [] } = options;
 
     eleventyConfig.addTransform('wcc', async (content, outputPath) => {
-      if(!outputPath.endsWith('.html')) {
+      if (!outputPath.endsWith('.html')) {
         return;
       }
 
       const { html } = await renderFromHTML(content, definitions);
 
       return html;
-    })
+    });
   }
-}
+};
