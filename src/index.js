025e658c1fc17dd569ed4b12441a353331bf05b5
Owen Buckley <owenbuckley13@gmail.com>
2022-06-26 13:30:30 -0400

    add require cache busting for live reload of custom definitions

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ const { renderFromHTML } = require('wc-compiler/dist/wcc.dist.cjs');
 module.exports = {
   configFunction: function (eleventyConfig, options = {}) {
     const { definitions = [] } = options;
+    const definitionPathnames = definitions.map(definition => definition.pathname);
+
+    for (const definition of definitions) {
+      eleventyConfig.addWatchTarget(definition.pathname);
+    }
 
     eleventyConfig.addTransform('wcc', async (content, outputPath) => {
       if (!outputPath.endsWith('.html')) {
@@ -12,6 +17,14 @@ module.exports = {
       const { html } = await renderFromHTML(content, definitions);
 
       return html;
+    });
+
+    eleventyConfig.on('eleventy.beforeWatch', async (changedFiles) => {
+      for(const file of changedFiles) {
+        if(definitionPathnames.includes(file)) {
+          delete require.cache[require.resolve(file)];
+        }
+      }
     });
   }
 };
