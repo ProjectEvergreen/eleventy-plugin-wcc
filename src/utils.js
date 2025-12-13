dda3162e31274c0a8f77b6831742fe8a5b0252a2
Kai Prince <34746763+KaiPrince@users.noreply.github.com>
2025-12-12 21:52:51 -0800

    tolerate whitespace

--- src/utils.js
+++ src/utils.js
@@ -1,15 +1,28 @@
 import * as parse5 from 'parse5';
 
+function isWhitespaceTextNode(node) {
+  return node.nodeName === '#text' && (/^[\t\n\r ]*$/).test(node.value);
+}
+
 export const stripWrappingParagraphs = (html) => {
   const parsedHtml = parse5.parseFragment(html);
   parsedHtml.childNodes = parsedHtml.childNodes.flatMap((node) => {
-    if (
-      node.nodeName === 'p' &&
-      node.childNodes.length === 1 &&
-      node.childNodes[0].nodeName.includes('-')
-    ) {
-      return node.childNodes[0];
+    if (node.nodeName !== 'p') {
+      return node;
     }
+
+    // Ignore whitespace-only text nodes
+    const meaningfulChildren = node.childNodes.filter(
+      (child) => !isWhitespaceTextNode(child)
+    );
+
+    if (
+      meaningfulChildren.length === 1 &&
+      meaningfulChildren[0].nodeName.includes('-')
+    ) {
+      return meaningfulChildren[0];
+    }
+
     return node;
   });
 
