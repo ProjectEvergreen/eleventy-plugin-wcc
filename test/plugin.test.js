669f10888e12a1921e7d27ceb7e72ec9bc872a91
Kai Prince <34746763+KaiPrince@users.noreply.github.com>
2025-12-31 17:06:08 -0800

    chore: #3 add basic tests (#20)
    * chore: Add basic tests
    
    Create plugin.test.js
    
    * Add test setup functions
    
    * Use setup helpers
    
    * Add Default options test group
    
    * Add test for trimParagraphTagsInMd: false
    
    * Override config path in tests
    
    * duplicate demo to test_fixtures folder
    
    * rename folder to test-fixtures

--- test/plugin.test.js
+++ test/plugin.test.js
@@ -1,22 +1,80 @@
 import Eleventy from '@11ty/eleventy';
 import { wccPlugin } from '../src/index.js';
 import assert from 'node:assert/strict';
-import { describe, it } from 'node:test';
+import { describe, it, before } from 'node:test';
 
 describe('WCC plugin', () => {
-  it('removes wrapping p tags', async () => {
-    const elev = new Eleventy('demo', 'test/output', {
-      config: function (eleventyConfig) {
-        eleventyConfig.addPlugin(wccPlugin.configFunction);
-      }
+  describe('Default options', () => {
+    let indexMdFile;
+
+    before(async () => {
+      const templates = await setUpTemplates();
+      ({ indexMdFile } = templates);
     });
 
-    await elev.init();
-    
-    const elevOutput = await elev.toJSON();
-    const result = elevOutput[0].content;
+    it('prints the header', () => {
+      const expected = '<h2>11ty + WCC Demo</h2>';
 
-    const regexp = new RegExp('<p><x-greeting>(.|\\s)*</x-greeting></p>');
-    assert.doesNotMatch(result, regexp);
+      assert.ok(indexMdFile.content.includes(expected), 'Header not found');
+    });
+
+    it('prints the greeting component', () => {
+      const contentNormalized = indexMdFile.content
+        .replaceAll(/\s+/g, ' ')
+        .trim();
+      const expected =
+        '<x-greeting><template shadowrootmode="open"> <p>Hello from the greeting component!</p> </template></x-greeting>';
+
+      assert.ok(
+        contentNormalized.includes(expected),
+        'x-greeting component not found'
+      );
+    });
+
+    it('removes wrapping p tags', () => {
+      const regexp = new RegExp('<p><x-greeting>(.|\\s)*</x-greeting></p>');
+
+      assert.doesNotMatch(indexMdFile.content, regexp);
+    });
+  });
+
+  describe('trimParagraphTagsInMd: false', () => {
+    it('leaves wrapping p tags', async () => {
+      const eleventy = setUpEleventy({ trimParagraphTagsInMd: false });
+      const { indexMdFile } = await setUpTemplates(eleventy);
+      const regexp = new RegExp('<p><x-greeting>(.|\\s)*</x-greeting></p>');
+
+      assert.match(indexMdFile.content, regexp);
+    });
   });
 });
+
+function setUpEleventy(pluginOptions = {}) {
+  return new Eleventy('test-fixtures', 'test/output', {
+    configPath: null,
+    config: function (eleventyConfig) {
+      eleventyConfig.addPlugin(wccPlugin, {
+        definitions: [
+          new URL('../test-fixtures/components/greeting.js', import.meta.url)
+        ],
+        ...pluginOptions
+      });
+    }
+  });
+}
+
+async function setUpTemplates(eleventy = null) {
+  eleventy ??= setUpEleventy();
+
+  await eleventy.init();
+  const elevOutput = await eleventy.toJSON();
+
+  const indexMdFile = elevOutput.find(
+    (template) => template.inputPath === './test-fixtures/index.md'
+  );
+
+  assert.ok(indexMdFile, 'Precondition failed: demo/index.md not found');
+
+  return { indexMdFile };
+}
+
