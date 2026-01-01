0c6f4bc44f0a7c9832b583f329909dcd5f68748e
Kai Prince <34746763+KaiPrince@users.noreply.github.com>
2025-12-31 17:15:33 -0800

    fix: #8 traverse child nodes when stripping p tags (#21)
    * Add test
    
    * fix: traverse child nodes when stripping p tags
    
    * Add additional test
    
    * fix: support full html doc
    
    * Add html doc tests

--- src/utils.js
+++ src/utils.js
@@ -5,26 +5,57 @@ function isWhitespaceTextNode(node) {
 }
 
 export const stripWrappingParagraphs = (html) => {
-  const parsedHtml = parse5.parseFragment(html);
-  parsedHtml.childNodes = parsedHtml.childNodes.flatMap((node) => {
-    if (node.nodeName !== 'p') {
-      return node;
-    }
+  const isFullHtmlDoc = (/^<(!DOCTYPE )?html>/i).test(html);
+  const parsedHtml = isFullHtmlDoc ? parse5.parse(html) : parse5.parseFragment(html);
 
-    // Ignore whitespace-only text nodes
-    const meaningfulChildren = node.childNodes.filter(
-      (child) => !isWhitespaceTextNode(child)
-    );
-
-    if (
-      meaningfulChildren.length === 1 &&
-      meaningfulChildren[0].nodeName.includes('-')
-    ) {
-      return meaningfulChildren[0];
-    }
-
-    return node;
-  });
+  const rootNode = chooseRootNode(parsedHtml);
+  rootNode.childNodes = rootNode.childNodes.map(traverseNodes);
 
   return parse5.serialize(parsedHtml);
 };
+
+function chooseRootNode(parsedHtml, isFullHtmlDoc) {
+  if (isFullHtmlDoc) {
+    const rootNode = parsedHtml.childNodes
+      .find((x) => x.nodeName === 'html')
+      ?.childNodes?.find((x) => x.nodeName === 'body');
+
+    if (!rootNode) {
+      throw new Error('html output is missing the body tag');
+    }
+
+    return rootNode;
+  } else {
+    return parsedHtml;
+  }
+}
+
+function traverseNodes(node) {
+  node = stripWrappingParagraph(node);
+
+  // Don't traverse children of custom elements
+  if (node.childNodes && !node.nodeName.includes('-')) {
+    node.childNodes = node.childNodes.map(traverseNodes);
+  }
+
+  return node;
+}
+
+function stripWrappingParagraph(node) {
+  if (node.nodeName !== 'p') {
+    return node;
+  }
+
+  // Ignore whitespace-only text nodes
+  const meaningfulChildren = node.childNodes.filter(
+    (child) => !isWhitespaceTextNode(child)
+  );
+
+  if (meaningfulChildren.length === 1 &&
+    meaningfulChildren[0].nodeName.includes('-')) {
+    return meaningfulChildren[0];
+  }
+
+  return node;
+}
+
